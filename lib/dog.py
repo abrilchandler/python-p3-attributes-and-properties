5fa6adb26b8e9a8e58ffe9cc6b211d76e01a912c
Chandler Abril <abrilchandler1@gmail.com>
2024-07-14 18:38:16 -0700

    Possible error in testing code

--- lib/dog.py
+++ lib/dog.py
@@ -14,28 +14,29 @@ APPROVED_BREEDS = [
 
 class Dog:
     
-    def __init__(self, name="Spot", breed="Mutt"):
-        #calling a method
-        self.name=(name) 
-        self.breed=(breed)
-#method name is .name and returns the value of _name which is the value of name
-    def get_name(self):
-        return self._name
-#method name is .name= and changes the value of _name 
-    def set_name(self, name):
+     def __init__(self, name="Spot", breed="Mutt"):
+         self.name = name 
+         self.breed = breed
+ #method name is .name and returns the value of _name which is the value of name
+     def get_name(self):
+         return self._name
+ #method name is .name= and changes the value of _name 
+     def set_name(self, name):
         if not isinstance(name, str) or len(name) < 1 or len(name) > 25:
             print("Name must be string between 1 and 25 characters.")
         else:
             self._name = name
-    name = property(get_name, set_name)
 
-    def get_breed(self):
-        return self._breed
 
-    def set_breed(self, breed):
-        if breed in APPROVED_BREEDS:
-           self._breed = breed
-        else:
+     def get_breed(self):
+         return self._breed
+
+     def set_breed(self, breed):
+         if breed not in APPROVED_BREEDS:
             print("Breed must be in list of approved breeds.")
+         else:
+             self._breed = breed
         
-    breed = property(get_breed, set_breed)
+   
+     name = property(get_name, set_name)
+     breed = property(get_breed, set_breed)
